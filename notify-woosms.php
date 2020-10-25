b65d5455906073a7088a161b6d4fa4b4665dbae5
Solayman Haider <solaymanhaider@gmail.com>
2020-10-26 05:01:20 +0600

    Update notify-woosms.php

--- notify-woosms.php
+++ notify-woosms.php
@@ -8,7 +8,7 @@ Plugin Name: Notify WooSMS
 Plugin URI: https://wordpress.org/plugins/notify-woosms/
 Description: WooCommerce order status sms notifications plugin for Bangladeshi SMS service providers.
 Author: Solayman Haider
-Version: 1.0
+Version: 2.0
 Author URI: https://solaymanhaider.com/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
